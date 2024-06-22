146e9c558007e9d5c1877c587b91c65020254feb
z1ndabad <taimur.samee@gmail.com>
2024-06-22 04:34:26 -0400

    tree rooting done

--- trees/rooting.py
+++ trees/rooting.py
@@ -2,4 +2,32 @@
 # Rooting a tree is assigning a specific node to be the root of a tree
 # and making all edges point away from the root
 
-import data
+import treedata
+
+
+# Note the visited collection isn't needed for tree DFS
+# Because the neighbor != parent check ensures the recursion ends
+def create_rooted_tree(
+    node_id, unrooted_tree: treedata.UnrootedTree, parent
+) -> treedata.TreeNode:
+    res = treedata.TreeNode(node_id, parent)
+
+    for neighbor in unrooted_tree.graph[node_id]:
+        if neighbor != parent:
+            res.children.append(create_rooted_tree(neighbor, unrooted_tree, node_id))
+
+    return res
+
+
+def print_rooted_tree(root: treedata.TreeNode):
+    print(root.id)
+    if not root.children:
+        return
+
+    for child in root.children:
+        print_rooted_tree(child)
+
+
+tree = treedata.UnrootedTree()
+result = create_rooted_tree(0, tree, None)
+print_rooted_tree(result)
---
+++ trees/treedata.py
@@ -0,0 +1,20 @@
+class UnrootedTree:
+    def __init__(self, graph=None) -> None:
+        if graph is None:
+            graph = {
+                0: [1, 2, 5],
+                1: [0],
+                2: [0, 3],
+                3: [2],
+                5: [0, 4, 6],
+                4: [5],
+                6: [5],
+            }
+        self.graph = graph
+
+
+class TreeNode:
+    def __init__(self, id, parent=None, children=None) -> None:
+        self.id = id
+        self.parent = None if parent == None else parent
+        self.children = [] if not children else children
