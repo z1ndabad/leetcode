3487d81b5f7ebb7ea7d7745d8bbd69af94a3f2f7
z1ndabad <taimur.samee@gmail.com>
2024-06-23 00:43:24 -0400

    find root fn

--- trees/rooting.py
+++ trees/rooting.py
@@ -3,18 +3,35 @@
 # and making all edges point away from the root
 
 import treedata
+import collections
 
 
 # Note the visited collection isn't needed for tree DFS
 # Because the neighbor != parent check ensures the recursion ends
-def create_rooted_tree(
-    node_id, unrooted_tree: treedata.UnrootedTree, parent
-) -> treedata.TreeNode:
+def find_root(graph: dict[int, list]):
+    leaves = collections.deque([node for node in graph if len(graph[node]) == 1])
+    degree = {node: len(graph[node]) for node in graph}
+    count = len(leaves)
+
+    while count < len(graph):
+        new_leaves = collections.deque()
+        for node in leaves:
+            degree[node] = 0
+            for neighbor in graph[node]:
+                degree[neighbor] -= 1
+                if degree[neighbor] == 1:
+                    new_leaves.append(neighbor)
+        count += len(new_leaves)
+        leaves = new_leaves
+    return leaves
+
+
+def create_rooted_tree(node_id, graph: dict[int, list], parent) -> treedata.TreeNode:
     res = treedata.TreeNode(node_id, parent)
 
-    for neighbor in unrooted_tree.graph[node_id]:
+    for neighbor in graph[node_id]:
         if neighbor != parent:
-            res.children.append(create_rooted_tree(neighbor, unrooted_tree, node_id))
+            res.children.append(create_rooted_tree(neighbor, graph, node_id))
 
     return res
 
@@ -29,5 +46,7 @@ def print_rooted_tree(root: treedata.TreeNode):
 
 
 tree = treedata.UnrootedTree()
-result = create_rooted_tree(0, tree, None)
+computed_root = find_root(tree.graph)
+print(list(computed_root))
+result = create_rooted_tree(0, tree.graph, None)
 print_rooted_tree(result)
