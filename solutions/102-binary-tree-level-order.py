e03beccf39a8ce4ab05b71048d1fcb69763d5cf5
z1ndabad <taimur.samee@gmail.com>
2024-07-08 20:03:50 -0400

    working level-order traversal

--- solutions/102-binary-tree-level-order.py
+++ solutions/102-binary-tree-level-order.py
@@ -1,31 +1,52 @@
-import math
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        queue = deque()
 
-def levelOrder(root):
-    level = 0
-    level_start = 0
-    res = []
+        if root:
+            queue.appendleft(root)
+        res = []
 
-    while level_start < len(root):
-        level_contents = []
-        level_start = int(math.pow(2, level) - 1)
-        level_size = int(math.pow(2, level))
+        while queue:
+            current_level = []
+            r = range(len(queue))
 
-        for i in range(level_start, level_start + level_size):
-            if i < len(root) and root[i]:
-                level_contents.append(root[i])
-        if level_contents:
-            res.append(level_contents)
+            for _ in r: 
+                node = queue.pop()
+                current_level.append(node.val)
+                
+                if node.left:
+                    queue.appendleft(node.left)
 
-        level += 1
+                if node.right:
+                    queue.appendleft(node.right)
 
-    return res
+            res.append(current_level)
+        return res
 
-
-input = [3, 9, 20, None, None, 15, 7]
-print(levelOrder(input))
-print(levelOrder([1]))
-print(levelOrder([]))
-
-# Outcome: correct for solution cases, but the input was not actually a list on leetcode
-# Need to redo for a binary tree object with left and right properties
+# Given the root of a binary tree (object), return the values
+# in the tree as a level-order traversal -- i.e. return
+# each layer of the tree as a subarray.
+#
+# Normal breadth-first traversal will visit all the levels in
+# order from the root. But there's no indicator of which level
+# the current node belongs to.
+#
+# NOTICE that if, at the start of the new level, the length of
+# the queue is the length of the level. So we can save the
+# entire level by popping the queue len(queue) times and saving
+# the values in a list. While doing this, we continue appending
+# each popped node's children to the queue, so once the for loop
+# breaks, the queue will contain all nodes in the NEXT level of
+# the tree.
+#
+# Inner for loop goes over each node in a level.
+#
+# Outer while loop goes through one level of the tree per iteration.
