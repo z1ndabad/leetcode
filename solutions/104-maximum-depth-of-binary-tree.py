bfb649d9866741250a16cd040e6f79a153a5d9ea
z1ndabad <taimur.samee@gmail.com>
2024-07-23 17:55:41 -0400

    treeeees

---
+++ solutions/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,75 @@
+from collections import deque
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+    def maxDepthIterative(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        depth = 0
+        
+        if root:
+            stack.append((1, root))
+
+        while stack:
+            current_depth, node = stack.pop()
+            depth = max(depth, current_depth)
+
+            if node.left:
+                stack.append((current_depth + 1, node.left))
+
+            if node.right:
+                stack.append((current_depth + 1, node.right))
+
+        return depth
+
+
+
+    def maxDepthBfs(self, root: Optional[TreeNode]) -> int:
+        queue = deque()
+        depth = 0
+
+        if root:
+            queue.appendleft(root)
+
+        while queue:
+            depth += 1
+            level = len(queue)
+            for _ in range(level):
+                node = queue.pop()
+
+                if node.left:
+                    queue.appendleft(node.left)
+
+                if node.right:
+                    queue.appendleft(node.right)
+
+        return depth
+
+# Given the root of a binary tree, return the maximum depth of the tree.
+# The depth of the root is 1.
+#
+# Remember tree depth is the longest path from the root to a leaf node.
+#
+# We can calculate this with any tree traversal:
+#
+# DFS recursive -- at a leaf node, return 0. Otherwise return the maximum
+# of maxDepth(left) and maxDepth(right) + 1
+#
+# DFS iterative -- modify the normal stack to contain a tuple of (depth, node)
+# where the depth of the root is 1. A node's children will have a depth of
+# current_depth + 1. Each iteration, if the depth of the node popped from the
+# stack exceeds the current max depth recorded, set max depth to the current
+# depth.
+#
+# BFS -- taking inspiration from 102. Binary Tree Level Order Traversal,
+# at the start of every iteration, the queue contains only nodes in the
+# current level of the tree. Use an inner for loop to pop and append children
+# len(queue) times, so all nodes from the current level are removed and all
+# children (next level) are added.
+#
+# Then every iteration of the outer while loop corresponds to a level of the
+# tree, so depth +=1 every iteration.
---
+++ solutions/226-invert-binary-tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = []
+        stack.append(root)
+
+        while stack:
+            node = stack.pop()
+
+            tmp = node.left
+            node.left = node.right
+            node.right = tmp
+
+            if node.right:
+                stack.append(node.right)
+
+            if node.left:
+                stack.append(node.left)
+
+        return root
+
+    def invertTreeRecursive(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        tmp = root.right
+        root.right = self.invertTreeRecursive(root.left)
+        root.left = self.invertTreeRecursive(tmp)
+
+        return root
+        
+
+# Given the root of a binary tree, invert the tree and return the root.
+# e.g. [2, 1, 3] --> [2, 3, 1]
+#
+# For every node, we need to swap the positions of its children, i.e.
+# node.left, node.right = node.right, node.left
+#
+# Can do this with any kind of graph traversal -- BFS, iterative DFS, recursive
+# DFS. See above for DFS examples.
